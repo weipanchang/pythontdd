50f922d6f75c4a7596c0d052c163c03ac0f072a9
weipanchang <weipanchang@hotmail.com>
2017-05-29 19:49:07 -0700

    add print out last inserted record

--- TorrentSearcher.py
+++ TorrentSearcher.py
@@ -32,18 +32,19 @@ def search_by_publisher(search_string, search_page_total, search_website):
                     cursor.execute(sql2, (string_list[1], string_list[0]))
                     result=cursor.fetchone()
                     if result[0] != 0:
-                         print string_list[1] + '\t\t' + string_list[0] + '\t\t' + 'already in database!'
+                         None
+#                         print string_list[1] + '\t\t' + string_list[0] + '\t\t' + 'already in database!'
                     else:
                          try:
                              # Execute the SQL command
                               cursor.execute(sql1, (string_list[1], string_list[0], search_string))
-                              print "insert successful"
+#                             print "insert successful"
                               # Commit your changes in the database
                               db.commit()
                          except:
                              # Rollback in case there is any error
                              db.rollback()
-                             print "Insert Failed"
+#                             print "Insert Failed"
 #                    print string_list[1] + '\t\t' + string_list[0]
      db.close()
      
@@ -83,13 +84,14 @@ def search_by_keyword(search_string, search_page_total, search_website):
                          cursor.execute(sql2, (string_list[1], string_list[0]))
                          result=cursor.fetchone()
                          if result[0] != 0:
-                              print string_list[1] + '\t\t' + string_list[0] + '\t\t' + 'already in database!'
+                              None
+#                              print string_list[1] + '\t\t' + string_list[0] + '\t\t' + 'already in database!'
                          else:
                               try:
                               # Execute the SQL command
                                    cursor.execute(sql1, (string_list[1], string_list[0], search_keyword.replace("-", " ")))
 #                                   print string_list[1] + '\t\t' + string_list[0]
-                                   print "Insert Successful"
+#                                   print "Insert Successful"
                                # Commit your changes in the database
                                    db.commit()
                               except:
@@ -101,19 +103,42 @@ def search_by_keyword(search_string, search_page_total, search_website):
      db.close()     
 
 if __name__ == "__main__":
-     string_list =["udemy","tutsplus", "tutplus","tuts+","oreilly","o'reilly","lynda", \
+     string_list =["udemy","tutsplus", "tutplus","tuts+" ,"tuts plus","oreilly","o'reilly","lynda", "apress", \
                    "cbt nuggest","pluralsight","learn","learning","raspberry pi", "itpro", "teamtreehouse"]
      search_website='https://limetorrents.cc'
      if len (sys.argv[1::]) <= 1:
 #     string_list =['udemy']
           for s in string_list:
                search_string=s.replace(' ', '-')
-               search_page_total = 2
+               search_page_total = 1
                search_by_publisher(search_string, search_page_total, search_website)
      else:
           search_string = sys.argv[1::]
           search_page_total = 1
           search_by_keyword(search_string, search_page_total, search_website)
+     db = MySQLdb.connect("localhost","root","abc123","Web_Scrape", unix_socket="/opt/lampp/var/mysql/mysql.sock" )
+     cursor = db.cursor()
+     sql1 = "SELECT @time := `date` FROM `WebUrl` group by `date` order by `date` DESC limit 1"
+     sql2 = "SELECT * FROM  `WebUrl` WHERE `date` = @time";
+     cursor.execute(sql1)
+     cursor.execute(sql2)
+     result = cursor.fetchall()
+     print ""
+     print ""
+     print ""
+     print "                The last inserted record!          "
+     print "================================================="
+     print ""
+     print ""
+     print ""
+     
+     for row in result:
+          print row[1] + "\t" + row[2] + "\t" + row[3]
+     db.close()
+     
+     
+
+
           
           
 
