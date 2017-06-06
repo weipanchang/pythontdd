6ebda63808077f216aafdfc16abeac8271f21a4b
weipanchang <weipanchang@hotmail.com>
2017-06-06 14:06:47 -0700

    absoulte rul

--- TorrentSearcher.py
+++ TorrentSearcher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import MySQLdb
+import urlparse
 import requests, urllib3, sys
 from bs4 import BeautifulSoup
 
@@ -29,7 +30,7 @@ def search_by_publisher(search_string, search_page_total, search_website):
                     string_list = str(i)[10:-4:].split('">')
                     sql1 = "INSERT INTO WebUrl(name, link, key_word) VALUES(%s, %s, %s)"
                     sql2 =  "SELECT count(*) FROM WebUrl where name = %s and link = %s"
-                    cursor.execute(sql2, (string_list[1], string_list[0]))
+                    cursor.execute(sql2, (string_list[1], urlparse.urljoin(search_website, string_list[0])))
                     result=cursor.fetchone()
                     if result[0] != 0:
                          None
@@ -37,7 +38,10 @@ def search_by_publisher(search_string, search_page_total, search_website):
                     else:
                          try:
                              # Execute the SQL command
-                              cursor.execute(sql1, (string_list[1], string_list[0], search_string))
+                              cursor.execute(sql1, (string_list[1], \
+                                                    urlparse.urljoin(search_website, string_list[0]), \
+                                                    search_string))
+#                              print string_list[1] + '\t\t' + urlparse.urljoin(search_website, string_list[0])
 #                             print "insert successful"
                               # Commit your changes in the database
                               db.commit()
@@ -75,13 +79,11 @@ def search_by_keyword(search_string, search_page_total, search_website):
                          if str2.find('html') == -1:
                               findit = False
                     if findit == True:
-                         # and ((str2)[10:10 + string_search_length:] == search_string \
-                         # or (str2)[11:11 + string_search_length:] == search_string):
                          string_list = str(i)[10:-4:].split('">')
                          print string_list
                          sql1 = "INSERT INTO WebUrl(name, link, key_word) VALUES(%s, %s, %s)"
                          sql2 =  "SELECT count(*) FROM WebUrl where name = %s and link = %s"
-                         cursor.execute(sql2, (string_list[1], string_list[0]))
+                         cursor.execute(sql2, (string_list[1], urlparse.urljoin(search_website, string_list[0])))
                          result=cursor.fetchone()
                          if result[0] != 0:
                               None
@@ -89,13 +91,15 @@ def search_by_keyword(search_string, search_page_total, search_website):
                          else:
                               try:
                               # Execute the SQL command
-                                   cursor.execute(sql1, (string_list[1], string_list[0], search_keyword.replace("-", " ")))
-#                                   print string_list[1] + '\t\t' + string_list[0]
+                                   cursor.execute(sql1, (string_list[1], \
+                                                       urlparse.urljoin(search_website, string_list[0]), \
+                                                       search_keyword.replace("-", " ")))
+#                                   print string_list[1] + '\t\t' + "https://www.limetorrents.cc/" + string_list[0]
 #                                   print "Insert Successful"
                                # Commit your changes in the database
                                    db.commit()
                               except:
-#                                   Rollback in case there is any error
+#                                  Rollback in case there is any error
                                    db.rollback()
 #                                  print string_list[1] + '\t\t' + string_list[0]
                                    print "Insert Failed"
@@ -103,23 +107,28 @@ def search_by_keyword(search_string, search_page_total, search_website):
      db.close()     
 
 if __name__ == "__main__":
+#     string_list =['udemy']
      string_list =["udemy","tutsplus", "tutplus","tuts+" ,"tuts plus","oreilly","o'reilly","lynda", "apress", \
-                   "cbt nuggest","pluralsight","learn","learning","raspberry pi", "itpro", "teamtreehouse"]
+                   "cbt nuggest","pluralsight","skillfeed", "learn","learning","raspberry pi", "raspberry-pi",\
+                   "itpro", "sams", "teamtreehouse", "livelessons"]
      search_website='https://limetorrents.cc'
      if len (sys.argv[1::]) <= 1:
 #     string_list =['udemy']
           for s in string_list:
                search_string=s.replace(' ', '-')
-               search_page_total = 1
+               search_page_total = 4
                search_by_publisher(search_string, search_page_total, search_website)
      else:
-          search_string = sys.argv[1::]
+          search_string = [i.lower() for i in sys.argv[1::]]
           search_page_total = 1
           search_by_keyword(search_string, search_page_total, search_website)
      db = MySQLdb.connect("localhost","root","abc123","Web_Scrape", unix_socket="/opt/lampp/var/mysql/mysql.sock" )
      cursor = db.cursor()
      sql1 = "SELECT @time := `date` FROM `WebUrl` group by `date` order by `date` DESC limit 1"
-     sql2 = "SELECT * FROM  `WebUrl` WHERE `date` = @time";
+     sql2 = "SELECT * FROM  `WebUrl` WHERE (`date` >= DATE_SUB(@time, INTERVAL 3 SECOND))";
+     sql3 = "UPDATE `WebUrl` SET `key_word` = 'tuts plus' WHERE `key_word` = 'tuts+' or \
+          `key_word` = 'tutplus'  or `key_word` = 'tut-plus'  or `key_word` = 'tutsplus'"
+     sql4 = "UPDATE `WebUrl` SET `key_word` = 'raspberry pi' WHERE `key_word` = 'raspberry-pi'"
      cursor.execute(sql1)
      cursor.execute(sql2)
      result = cursor.fetchall()
@@ -129,11 +138,14 @@ if __name__ == "__main__":
      print "                The last inserted record!          "
      print "================================================="
      print ""
-     print ""
-     print ""
-     
      for row in result:
-          print row[1] + "\t" + row[2] + "\t" + row[3]
+          print row[1] + "\t" + urlparse.urljoin(search_website, row[2]) + "\t" + row[3]
+          print ""
+     print ""
+     print "The last timestamp is ====>    " + str(row[4])
+     cursor.execute(sql3)
+     cursor.execute(sql4)
+     db.commit()
      db.close()
      
      
